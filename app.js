3b1f42f0ff4c1bd4a11bf0008dfc156f287dc76a
ruan <ruandasilvasantos49@gmail.com>
2025-03-19 22:44:37 -0300

    Desafio 2: imagem, scrollbar, botoes de cancelar e inscrição, escolha de trilhas

--- app.js
+++ app.js
@@ -8,40 +8,40 @@ let rua = document.getElementById("label-rua");
 rua.textContent = "Rua";
 let numero = document.getElementById("label-numero");
 numero.textContent = "Número";
-let cidade =  document.getElementById("label-cidade");
+let cidade = document.getElementById("label-cidade");
 cidade.textContent = "Cidade";
-let estado =  document.getElementById("label-estado");
-estado.textContent = "Estado";  
+let estado = document.getElementById("label-estado");
+estado.textContent = "Estado";
 
 //selecionar o arquivo
-function selecionarArquivo(){
+function selecionarArquivo() {
     console.log("função chamada")
     let arquivo = document.querySelector('.file-upload-area');
-        
-        if(arquivo){
-           arquivo.addEventListener('click', function(){
+
+    if (arquivo) {
+        arquivo.addEventListener('click', function () {
             console.log("Area de arquivo foi clicada");
             document.getElementById('file-input').click();
-           });
-        }else{
-            console.log("missão falhou: elemento file-upload-are não encotrado");
-        }
-  
+        });
+    } else {
+        console.log("missão falhou: elemento file-upload-are não encotrado");
+    }
+
 }
 selecionarArquivo();  //chamada da função
 
 //funçao que altera a cor dos campos de entradas
-function alterarCor(id){
+function alterarCor(id) {
     let entrada = document.getElementById(`entrada-${id}`);
-    entrada.addEventListener('focus', function(){
+    entrada.addEventListener('focus', function () {
         entrada.classList.remove('bg-secondary', 'bg-opacity-25');
         entrada.classList.add('bg-light')
     });
-    entrada.addEventListener('blur', function() {
+    entrada.addEventListener('blur', function () {
         if (!entrada.value) {
             entrada.classList.remove('bg-light');
             entrada.classList.add('bg-secondary', 'bg-opacity-25');
-        }else {
+        } else {
             console.error(`Elemento com id entrada-${id} não encontrado!`);
         }
     });
@@ -67,3 +67,23 @@ alterarCor(11);
 //lógica e DOM do section trilhas 
 let tituloTrilhas = document.getElementById("title-trilhas");
 tituloTrilhas.textContent = "Trilhas de Aprendizagem";
+
+// Seleciona a caixa de "Programação Front-end"
+function trilhasCheck() {
+    let trilhas = document.querySelectorAll('input[name="trilhas"]');
+
+    trilhas.forEach(input => {
+        input.addEventListener('change', () => {
+            if (input.checked) {
+                let trilhaSelecionada = input.closest('.icons').querySelector('span').textContent.trim();
+                console.log(`Usuario clicou em : ${trilhaSelecionada}`);
+            }
+        });
+    });
+}
+
+trilhasCheck();
+
+
+
+  
